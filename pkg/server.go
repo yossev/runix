d70362c9d82bd53aa6d58f8f5d4bd5bf7a5db230
Yossev <synthpyt@gmail.com>
2024-07-25 18:33:49 +0300

    Temp fix for packages

--- pkg/server.go
+++ pkg/server.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
-    "runix/pkg/api"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    r := mux.NewRouter()
-    
-    // Set up API routes
-    api.SetupRoutes(r)
-    
-    // Start the server
-    http.Handle("/execute", r)
-    log.Println("Server is running on port 8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatalf("Server failed: %v", err)
-    }
+	r := mux.NewRouter()
+
+	// Set up API routes
+	api.SetupRoutes(r)
+
+	// Start the server
+	http.Handle("/execute", r)
+	log.Println("Server is running on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
